16e2b74afaf0cd351c64f2822defc909caad752d
Mark Lyck <mark.lyck@gmail.com>
2016-08-18 10:30:29 -0500

    suggestions now restricted to users plan

--- app/scripts/components/dashboard/Suggestions.js
+++ app/scripts/components/dashboard/Suggestions.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 import Suggestion from './Suggestion'
 import SuggestionHeader from './SuggestionHeader'
@@ -27,18 +28,32 @@ const Suggestions = React.createClass({
     store.plans.get('fund').off('change', this.updateState)
   },
   render() {
-    let suggestions = store.plans.get(this.props.plan).get('suggestions').map((suggestion, i) => {
-      return (
-        <Suggestion key={i} suggestion={suggestion}/>
+
+    let suggestionsList;
+    if(store.session.isAllowedToView(this.props.plan)) {
+      let suggestions = store.plans.get(this.props.plan).get('suggestions').map((suggestion, i) => {
+        return (
+          <Suggestion key={i} suggestion={suggestion}/>
+        )
+      })
+      suggestionsList = (
+        <ul className="suggestions-list">
+          {suggestions}
+        </ul>
       )
-    })
+    } else {
+      suggestionsList = (
+        <section className="no-permissions">
+          <h3>Upgrade to the <span className="capitalize blue-color ">{this.props.plan} formula</span> to see these suggestions</h3>
+          <Link to="/dashboard/account" className="filled-btn">Upgrade your plan</Link>
+        </section>
+      )
+    }
 
     return (
       <div className="suggestions">
         <SuggestionHeader plan={this.props.plan}/>
-        <ul className="suggestions-list">
-          {suggestions}
-        </ul>
+        {suggestionsList}
       </div>
     )
   }
