bd9f912a9e913d2bdf44aa705a5e2bfa1eec78ea
Mark Lyck <mark.lyck@gmail.com>
2016-08-19 16:29:26 -0500

    pricing table

--- app/scripts/models/Visit.js
+++ app/scripts/models/Visit.js
@@ -8,14 +8,28 @@ const Visit = Backbone.Model.extend({
     device: '',
     location: {},
     browser: '',
-    type: -1
+    type: -1,
+
   },
   getData(type) {
     $.ajax('https://freegeoip.net/json/')
     .then((r) => {
+      console.log(r.ip);
+      let str = r.ip.replace('.', '')
+      // this.set('id', r.ip)
+      this.set('_id', str)
       this.set('location', r)
       this.set('type', type || -1)
-      this.save()
+      if (this.get('type') === -1) {
+        this.save(null, {
+          url: `https://baas.kinvey.com/appdata/kid_rJRC6m9F/visits`,
+          type: 'POST',
+          error: (e) => {
+            console.error('failed posting visit: ', e)
+          }
+        })
+      }
+
     })
   }
 })
