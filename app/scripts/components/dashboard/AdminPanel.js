4ace334e49705892bcd2849e9a9a29134c5bb3e5
Mark Lyck <mark.lyck@gmail.com>
2016-08-20 00:23:18 -0500

    Updated map to make more sense with unique users

--- app/scripts/components/dashboard/AdminPanel.js
+++ app/scripts/components/dashboard/AdminPanel.js
@@ -21,51 +21,54 @@ const AdminPanel = React.createClass({
   },
   render() {
     // console.log(this.state.visitors);
-    let fixedVisitors = this.state.visitors.reduce((returnSoFar, visitor) => {
-      visitor.amount = 1
-      let index = 0
-      let duplicates = returnSoFar.filter((vtor, i) => {
-        if (vtor.location.city === visitor.location.city) {
-          index = i
-          return true
-        }
-      })
-
-      if (duplicates.length === 0) {
-        returnSoFar.push(visitor)
-      } else {
-        console.log('else running');
-        let newVisitor = returnSoFar[index]
-        newVisitor.amount += 1
-        returnSoFar = returnSoFar.splice(0,index).concat(newVisitor, returnSoFar.splice(index + 1))
-      }
-      return returnSoFar
-    }, [])
+    // let fixedVisitors = this.state.visitors.reduce((returnSoFar, visitor) => {
+    //   visitor.amount = 1
+    //   let index = 0
+    //   let duplicates = returnSoFar.filter((vtor, i) => {
+    //     if (vtor.location.city === visitor.location.city) {
+    //       index = i
+    //       return true
+    //     }
+    //   })
+    //
+    //   if (duplicates.length === 0) {
+    //     returnSoFar.push(visitor)
+    //   } else {
+    //     console.log('else running');
+    //     let newVisitor = returnSoFar[index]
+    //     newVisitor.amount += 1
+    //     returnSoFar = returnSoFar.splice(0,index).concat(newVisitor, returnSoFar.splice(index + 1))
+    //   }
+    //   return returnSoFar
+    // }, [])
     // console.log('fixed: ', fixedVisitors);
 
-    let max = 1;
+    // let max = 1;
     // let min = 15;
 
-    let images = fixedVisitors.map((visitor) => {
+    let images = this.state.visitors.map((visitor) => {
 
-      if (visitor.amount > max) {
-        max = visitor.amount
-      }
-      let size = visitor.amount / max * 20
-      if (size <= 5) {
-        size = 5
-      }
+      // if (visitor.amount > max) {
+      //   max = visitor.amount
+      // }
+      // let size = visitor.amount / max * 20
+      // if (size <= 5) {
+      //   size = 5
+      // }
+
+      console.log(visitor);
+      let color = (visitor.type > 0 && visitor.type < 5) ? '#27A5F9' : visitor.type === 5 ? '#da1354' : '#12D99E'
 
       return({
         "type": "circle",
         "theme": "light",
 
-        "width": size,
-        "height": size,
-        "color": '#27A5F9',
+        "width": 5,
+        "height": 5,
+        "color": color,
         "longitude": visitor.location.longitude,
         "latitude": visitor.location.latitude,
-        "title": `${visitor.location.country_name}<br/>${visitor.location.city}<br/>${visitor.amount}`,
+        "title": `${visitor.location.country_name}<br/>${visitor.location.city}<br/>`,
         "value": visitor.amount
       });
     })
