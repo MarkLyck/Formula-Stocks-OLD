1a97a4662c3028b5969a34f0749d3384d501f908
Mark Lyck <mark.lyck@gmail.com>
2016-09-07 17:05:51 -0500

    updated text in pricing table

--- app/scripts/components/home/Product.js
+++ app/scripts/components/home/Product.js
@@ -22,9 +22,9 @@ const Product = React.createClass({
           {extraInfo}
           <p>Buy & sell recommendations</p>
           <p>Model Portfolio Tracking</p>
-          <p><span className="light-text-color">Avg. round trip trades per year: </span>{this.props.info.roundtripTradesPerYear}</p>
+          <p><span className="light-text-color">Avg. round trip trades p.a: </span>{this.props.info.roundtripTradesPerYear}</p>
           <p><span className="light-text-color">IIT formulas applied: </span>{this.props.info.IITFormulas}</p>
-          <p><span className="light-text-color">Historical 45-year CAGR: </span>{this.props.stats.CAGR.toFixed(2)}%</p>
+          <p><span className="light-text-color">Historical CAGR: </span>{this.props.stats.CAGR.toFixed(2)}%</p>
           <p><span className="light-text-color">Avg. winning positions: </span>{this.props.stats.WLRatio.toFixed(2)}%</p>
         </div>
         <button onClick={this.showModal} className="more-info filled-btn">More info</button>
