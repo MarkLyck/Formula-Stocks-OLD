af16aa5d150595f8617879749a4501b082051301
Mark Lyck <mark.lyck@gmail.com>
2016-09-14 12:59:21 -0500

    updated our product section

--- app/scripts/components/home/OurProducts.js
+++ app/scripts/components/home/OurProducts.js
@@ -20,13 +20,13 @@ const OurProducts = React.createClass({
           <p>
           We offer four products, each of which gives you access to a secure dashboard.
           You will be able to track our model portfolio, access our purchase and
-          sales transactions in real-time as they occur,
+          sales transactions in real-time,
           and obtain valuable information about stocks selected for purchase or sale.<br/><br/>
 
 
-          You can choose to use this information to buy stocks through your regular broker/bank,
+          You can use this information to buy stocks through your regular broker/bank,
           either by using our selections on an ongoing basis, or by mirroring the model portfolio
-          and keeping abreast of changes as they regularly occur.<br/><br/>
+          and keeping abreast of changes.<br/><br/>
 
           Once you have made your purchases, you simply wait. Later,
           a sales recommendation will suggest when to sell the stock.
@@ -37,33 +37,36 @@ const OurProducts = React.createClass({
             <div className="plan-icon">
               <img src="assets/icons/icon-user.svg"/>
               <p className="blue-color">Basic</p>
+              <p className="descr">
+                Basic is a diversified retail product with an excellent value proposition, targeted at the private investor. Excellent for the beginner.
+              </p>
             </div>
             <div className="plan-icon">
               <img src="assets/icons/icon-briefcase.svg"/>
               <p className="blue-color">Premium</p>
+              <p className="descr">
+                Premium is for the performance oriented private investor who wants the added value of higher returns and much higher win rates.
+              </p>
             </div>
             <div className="plan-icon">
               <img src="assets/icons/icon-chart.svg"/>
               <p className="blue-color">Business</p>
+              <p className="descr">
+                Business is designed for CEOs, accredited investors, and enterprising investors focusing on strong performance and margin of safety, at the expense of diversification.
+              </p>
             </div>
             <div className="plan-icon">
               <img src="assets/icons/icon-building.svg"/>
               <p className="blue-color">Fund</p>
+              <p className="descr">
+                Fund is for institutional capital, capable of handling large AUMs, which other products cannot. It provides stronger diversification and much deeper liquidity.
+              </p>
             </div>
           </div>
-
           <p>
-          <span className="bold">Basic</span> and <span className="bold">Premium</span> are both diversified retail products with excellent value propositions and targeted at the private investor.
-          <span className="bold"> Business</span> is designed for CEOs, accredited investors, and enterprising investors focusing on performance and margin of safety,
-          at the expense of diversification. Our <span className="bold">Fund</span> product is for institutional capital,
-          capable of handling large diversified AUMs, which other products cannot.
-           Each product utilizes a different number of Intelligent Investment Technology formulas.
-          You can explore our <ScrollLink className="blue-color" to="pricing" smooth={true} offset={-100} duration={1000}>product matrix</ScrollLink> below.
-          Or see our <a className="blue-color" onClick={this.downloadBrochure.bind(null, 'FAQ')}>FAQ</a> or <a className="blue-color" onClick={this.downloadBrochure.bind(null,'business')}>business brochure</a>.<br/><br/>
-
-          <span className="disclaimer">Formula Stocks does not provide personalized investment advice. We respect your privacy
-          and receive no financial information in the process.
-          We do not manage money or recommend stocks with which we have any affiliation.</span>
+            Each product utilizes a different number of Intelligent Investment Technology formulas.
+            You can explore our <ScrollLink className="blue-color" to="pricing" smooth={true} offset={-100} duration={1000}>product matrix</ScrollLink> below.
+            Or see our <a className="blue-color" onClick={this.downloadBrochure.bind(null, 'FAQ')}>FAQ</a> or <a className="blue-color" onClick={this.downloadBrochure.bind(null,'business')}>business brochure</a>.<br/><br/>
           </p>
         </div>
       </section>
