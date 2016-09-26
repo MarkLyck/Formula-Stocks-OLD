609e325424300168c0a0665fc1c921f4f54ea614
Mark Lyck <mark.lyck@gmail.com>
2016-09-25 23:10:03 -0500

    Fsdemo now has permission to see all plans in the dashboard

--- app/scripts/models/Session.js
+++ app/scripts/models/Session.js
@@ -161,6 +161,10 @@ const Session = Backbone.Model.extend({
     }
   },
   isAllowedToView(plan) {
+    console.log('this: ', this);
+    if (this.get('email') === 'demo@formulastocks.com') {
+      return true
+    }
     let type = 5
     if (plan === 'basic')         { type = 1 }
     else if (plan === 'premium')  { type = 2 }
