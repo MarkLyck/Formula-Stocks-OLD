eabe01b03902fcd67fb3b29492b33dfbb56f41ea
Mark Lyck <mark.lyck@gmail.com>
2016-10-21 09:15:26 -0500

    Update a subtitle

--- app/scripts/components/home/Hero.js
+++ app/scripts/components/home/Hero.js
@@ -41,7 +41,7 @@ const Hero = React.createClass({
               <h2>The human brain is not wired for evaluating hundreds of data points simultaneously. It prefers stories. But stories produce bias that dampens performance.</h2>
               <h2>Formula Stocks' intelligent technology delivers unbiased thinking on an expert level. Smart. Experienced. Better.</h2>
               <h2>Expert systems can evaluate hundreds of data points simultaneously. Use this as a tool to improve your odds.</h2>
-              <h2>With this tool, it becomes possible to win 9 times out of 10.</h2>
+              <h2>With this tool, it becomes possible to win up to 9 times out of 10.</h2>
               <h2>Based on timeless and proven investment principles. We buy good businesses at reasonable prices, using a margin of safety.</h2>
               <h2>Progress always involves adopting better tools.</h2>
             </div>
