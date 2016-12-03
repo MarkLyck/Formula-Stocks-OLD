fd40f4f82b2923068650290b951068066c42d235
Mark Lyck <mark.lyck@gmail.com>
2016-12-03 17:49:39 +0100

    Business brochure button fix

--- app/scripts/components/home/Brochures.js
+++ app/scripts/components/home/Brochures.js
@@ -12,7 +12,12 @@ const Brochures = React.createClass({
         <h2 className="title">Want more information? Download our brochures</h2>
         <div className="cta">
           <Link to="/faq" className="filled-btn"><i className="fa fa-file" aria-hidden="true"></i>FAQ</Link>
-          <button onClick={this.downloadBrochure.bind(null, 'business')} className="filled-btn"><i className="fa fa-file" aria-hidden="true"></i>Business brochure</button>
+          <button onClick={this.downloadBrochure.bind(null, 'business')} className="filled-btn">
+            <div className="button-content">
+              <i className="fa fa-file" aria-hidden="true"></i>
+              <p>Business brochure</p>
+            </div>
+          </button>
         </div>
       </section>
     )
