5e2fb3497ea8276bf7a311a0905e3520db242cf9
Mark Lyck <mark.lyck@gmail.com>
2016-12-14 22:05:51 +0100

    Update Visitors graph

--- app/scripts/components/dashboard/DAUGraph.js
+++ app/scripts/components/dashboard/DAUGraph.js
@@ -9,52 +9,63 @@ const DAUGraph = React.createClass({
     let visitors = this.props.data
 
     visitors = visitors.map(visitor => {
-      return moment(visitor._kmd.lmt).format('YYYY-MM-DD')
+      return  {
+                firstVisit: moment(visitor._kmd.ect).format('YYYY-MM-DD'),
+                lastVisit: moment(visitor._kmd.lmt).format('YYYY-MM-DD')
+              }
     })
 
     visitors = _.sortBy(visitors, (visitor) => {
-      return Number(visitor.split('-').join(''))
+      return Number(visitor.firstVisit.split('-').join(''))
     })
 
-
-    visitors = visitors.reduce((prev, curr) => {
+    let chartData = visitors.reduce((prev, curr) => {
 
       let dateFound = false
+
       prev = prev.map(point => {
-        if (point.date === curr) {
+        if (point.date === curr.firstVisit) {
           dateFound = true
-          point.visitors++
+          point.newVisitors++
         }
         return point
       })
+
       if (prev[prev.length - 1] ) {
-        let currDate = Number(curr.split('-').join(''))
+        let currDate = Number(curr.firstVisit.split('-').join(''))
         let prevDate = Number(prev[prev.length - 1].date.split('-').join(''))
 
         const missingDates = currDate - prevDate
 
         if (missingDates > 1 && missingDates < 28) {
-            const year = curr.split('-')[0]
-            const month = curr.split('-')[1]
+            const year = curr.firstVisit.split('-')[0]
+            const month = curr.firstVisit.split('-')[1]
             let date = Number(prev[prev.length - 1].date.split('-')[2])
+
           _(missingDates - 1).times(() => {
             date++
-            prev = prev.concat({ visitors: 0, date: `${year}-${month}-${date}` })
+            prev = prev.concat({
+              newVisitors: 0,
+              recurrentVisitors: 0,
+              date: `${year}-${month}-${date}` })
           })
         }
       }
-
-
       if (!dateFound) {
-        return prev.concat({ visitors: 1, date: curr })
+        return prev.concat({ newVisitors: 1, date: curr.firstVisit, recurrentVisitors: 0 })
       } else {
-
-
         return prev
       }
     }, [])
 
-    let chartData = visitors
+    chartData = chartData.map(point => {
+      visitors.forEach(visitor => {
+        if (visitor.lastVisit === point.date && visitor.lastVisit !== visitor.firstVisit) {
+          point.recurrentVisitors++
+        }
+      })
+      return point
+    })
 
     let chartTheme =  'light'
     let gridOpacity = 0.05
@@ -85,14 +96,24 @@ const DAUGraph = React.createClass({
       },
       "graphs": [
           {
-            "id": "DAU",
-            lineColor: '#27A5F9',
+            "id": "firstVisits",
+            lineColor: '#12D99E',
             "lineThickness": 2,
-            "valueField": "visitors",
+            "valueField": "newVisitors",
             // "type": "smoothedLine",
             // // "bullet": "round",
             // // "bulletSize": 3,
-            "balloonText": `<div class=\"suggestion-balloon\"><p class="ticker">Visitors</p> <p>[[value]]</p></div>`
+            "balloonText": `<div class=\"suggestion-balloon\"><p class="ticker">New visitors</p> <p>[[value]]</p></div>`
+        },
+        {
+          "id": "recVisitors",
+          lineColor: '#27A5F9',
+          "lineThickness": 2,
+          "valueField": "recurrentVisitors",
+          // "type": "smoothedLine",
+          // // "bullet": "round",
+          // // "bulletSize": 3,
+          "balloonText": `<div class=\"suggestion-balloon\"><p class="ticker">Recurrent visitors</p> <p>[[value]]</p></div>`
         }
       ],
       chartCursor: {
