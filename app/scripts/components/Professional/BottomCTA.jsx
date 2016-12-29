42325d98922203b33be8ffe5e917516085820501
Mark Lyck <mark.lyck@gmail.com>
2016-12-29 13:31:07 +0100

    Bottom CTA

--- app/scripts/components/Professional/BottomCTA.jsx
+++ app/scripts/components/Professional/BottomCTA.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 class BottomCTA extends React.Component {
   render() {
     return (
-      <section>
+      <section className="bottom-cta">
         <h1>Now that you've scrolled all the way to the bottom...</h1>
         <h3>It can be just the right moment to stop reading and do some clicking instead.</h3>
-        <button>I'm ready to try</button>
-        <a>Still not Convinced? Download our brochure.</a>
+        <button className="to-pricing">I'm ready to try</button>
+        <a className="not-convinced">Still not Convinced? Download our brochure.</a>
       </section>
     )
   }
