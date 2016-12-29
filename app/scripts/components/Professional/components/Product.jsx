267dd5921534d19123c26152b7af8c9ce7ac7b5a
Mark Lyck <mark.lyck@gmail.com>
2016-12-29 12:33:06 +0100

    Product stats rearrangement

--- app/scripts/components/Professional/components/Product.jsx
+++ app/scripts/components/Professional/components/Product.jsx
@@ -9,6 +9,7 @@ function formatPrice(value) {
 }
 
 const Product = ({ plan, billed }) => {
+  let extraInfo = plan.name === 'business' ? <p>Includes Premium</p> : plan.name === 'fund' ? <p>AUM capacity: $300 billion</p> : ''
   return (
   <div className="prof-plan">
     <div className="top">
@@ -18,13 +19,14 @@ const Product = ({ plan, billed }) => {
     <ul className="statistics">
       <p>Buy & sell recommendations</p>
       <p>Model portfolio</p>
+      {extraInfo}
       <p><span className="light-text-color">IIT formulas applied: </span>{plan.info.IITFormulas}</p>
       <p><span className="light-text-color">Annual growth: </span>{plan.stats.CAGR.toFixed(2)}%</p>
       <p><span className="light-text-color">Win/loss ratio: </span>{plan.stats.WLRatio.toFixed(2)}%</p>
-      <p><span className="light-text-color">Avg. round trip trades p.a: </span>{plan.info.roundtripTradesPerYear}</p>
-      <p><span className="light-text-color">Avg. number of pos. in portfolio: </span>{plan.info.avgNumOfPosInPortfolio}</p>
       <p><span className="light-text-color">Avg. gain per position: </span>{plan.info.avgGainPerPosition}%</p>
       <p><span className="light-text-color">Avg. loss per position: </span>{plan.info.avgLossPerPosition}%</p>
+      <p><span className="light-text-color">Avg. round trip trades p.a: </span>{plan.info.roundtripTradesPerYear}</p>
+      <p><span className="light-text-color">Avg. number of pos. in portfolio: </span>{plan.info.avgNumOfPosInPortfolio}</p>
       <p><span className="light-text-color">Max drawdown in 45 years: </span>{plan.info.maxDrawdown45y}%</p>
       <p><span className="light-text-color">Max drawdown in 36 months: </span>{plan.info.maxDrawdown36m}%</p>
       <p><span className="light-text-color">IRR Arithmetic mean: </span>{plan.info.IRRArithmeticMean}%</p>
