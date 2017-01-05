19079d76aed4c8e0ecb9e13950e1b79643e46183
Mark Lyck <mark.lyck@gmail.com>
2017-01-05 02:11:24 +0100

    Billing responsive design

--- app/scripts/components/Professional/Modals/SignUp/SignUp.jsx
+++ app/scripts/components/Professional/Modals/SignUp/SignUp.jsx
@@ -25,7 +25,7 @@ class SignUp extends React.Component {
     this.lastPage = this.lastPage.bind(this)
 
     this.state = {
-      page: 1,
+      page: 2,
       selected: 'premium',
       gotInfo: false
     }
