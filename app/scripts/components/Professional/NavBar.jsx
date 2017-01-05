efd350f1e771f89725f2cd8de2ecc9e37f2804c2
Mark Lyck <mark.lyck@gmail.com>
2017-01-05 23:31:53 +0100

    Mobile navbar

--- app/scripts/components/Professional/NavBar.jsx
+++ app/scripts/components/Professional/NavBar.jsx
@@ -10,7 +10,11 @@ const scroll = Scroll.animateScroll
 class NavBar extends React.Component {
   constructor() {
     super()
-    this.state = { navbar: 1 }
+    this.toggleMenu = this.toggleMenu.bind(this)
+    this.closeMenu = this.closeMenu.bind(this)
+    this.renderMenu = this.renderMenu.bind(this)
+
+    this.state = { navbar: 1, showMenu: false }
   }
 
   componentDidMount() {
@@ -45,18 +49,35 @@ class NavBar extends React.Component {
     }
   }
 
+  toggleMenu() {
+    this.setState({ showMenu: !this.state.showMenu })
+  }
+  closeMenu() {
+    this.setState({ showMenu: false })
+  }
+
+  renderMenu() {
+    if (this.state.showMenu) {
+      return <button className="menu-btn" onClick={this.toggleMenu}><i className="fa fa-times" aria-hidden="true"></i></button>
+    } else {
+      return <button className="menu-btn" onClick={this.toggleMenu}><i className="fa fa-bars" aria-hidden="true"></i></button>
+    }
+  }
+
   render() {
+    let menuState = this.state.showMenu ? 'right open' : 'right closed'
     if (this.state.navbar === 1) {
       return (
         <nav className="prof-navbar static">
           <div className="left" onClick={() => {scroll.scrollToTop()}}>
             <a><img src="/assets/images/logo_horizontal.svg"/></a>
           </div>
-          <div className="right">
-            <ScrollLink className="nav-link overview" to="whatIsIt" smooth={true} offset={-40} duration={1000}>Overview</ScrollLink>
-            <ScrollLink className="nav-link performance" to="performance" smooth={true} offset={-100} duration={1000}>Performance</ScrollLink>
-            <ScrollLink className="nav-link how-it-works" to="howItWorks" smooth={true} offset={-100} duration={1000}>How it works</ScrollLink>
-            <ScrollLink className="nav-link pricing" to="pricing" smooth={true} offset={-100} duration={1000}>Pricing</ScrollLink>
+          {this.renderMenu()}
+          <div className={menuState}>
+            <ScrollLink onClick={this.closeMenu} className="nav-link overview" to="whatIsIt" smooth={true} offset={-40} duration={1000}>Overview</ScrollLink>
+            <ScrollLink onClick={this.closeMenu} className="nav-link performance" to="performance" smooth={true} offset={-100} duration={1000}>Performance</ScrollLink>
+            <ScrollLink onClick={this.closeMenu} className="nav-link how-it-works" to="howItWorks" smooth={true} offset={-100} duration={1000}>How it works</ScrollLink>
+            <ScrollLink onClick={this.closeMenu} className="nav-link pricing" to="pricing" smooth={true} offset={-100} duration={1000}>Pricing</ScrollLink>
             {this.renderUserLinks()}
           </div>
         </nav>
@@ -67,12 +88,13 @@ class NavBar extends React.Component {
           <div className="left" onClick={() => {scroll.scrollToTop()}}>
             <a><img src="/assets/images/logo_horizontal.svg"/></a>
           </div>
-          <div className="right">
-            <ScrollLink className="nav-link overview" to="whatIsIt" smooth={true} offset={-100} duration={1000}>Overview</ScrollLink>
-            <ScrollLink className="nav-link performance" to="performance" smooth={true} offset={-100} duration={1000}>Performance</ScrollLink>
-            <ScrollLink className="nav-link how-it-works" to="howItWorks" smooth={true} offset={-100} duration={1000}>How it works</ScrollLink>
-            <Link className="nav-link faq-link" to="/faq" >FAQ</Link>
-            <ScrollLink className="nav-link pricing" to="pricing" smooth={true} offset={-100} duration={1000}>View pricing</ScrollLink>
+          {this.renderMenu()}
+          <div className={menuState}>
+            <ScrollLink onClick={this.closeMenu} className="nav-link overview" to="whatIsIt" smooth={true} offset={-100} duration={1000}>Overview</ScrollLink>
+            <ScrollLink onClick={this.closeMenu} className="nav-link performance" to="performance" smooth={true} offset={-100} duration={1000}>Performance</ScrollLink>
+            <ScrollLink onClick={this.closeMenu} className="nav-link how-it-works" to="howItWorks" smooth={true} offset={-100} duration={1000}>How it works</ScrollLink>
+            <Link onClick={this.closeMenu} className="nav-link faq-link" to="/faq" >FAQ</Link>
+            <ScrollLink onClick={this.closeMenu} className="nav-link pricing" to="pricing" smooth={true} offset={-100} duration={1000}>View pricing</ScrollLink>
             {this.renderUserLinks()}
           </div>
         </nav>
