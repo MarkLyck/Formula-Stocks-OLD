0eef8aeb10133e1193af45bc0633a00b45ce8f54
Mark Lyck <mark.lyck@gmail.com>
2017-01-07 19:03:41 +0100

    Remove unused scripts

--- app/scripts/components/home/Home.js
+++ app/scripts/components/home/Home.js
@@ -3,34 +3,23 @@ import React from 'React'
 import store from '../../store'
 
 import NavBar from '../global/NavBar.jsx'
-// import Hero from './Hero'
 import Hero from '../global/Hero.jsx'
-import AboveAverageReturns from './fiveReasons/AboveAverageReturns'
-import ReachYourGoals from './fiveReasons/ReachYourGoals'
-import InformationalAdvantage from './fiveReasons/InformationalAdvantage'
 import OneDollar from './OneDollar'
-import HigherPerformance from './fiveReasons/HigherPerformance'
 import WhatIsIt from './WhatIsIt.jsx'
 import HowItWorks from './HowItWorks.jsx'
 import PilotTest from './PilotTest.jsx'
 import Performance from '../global/Performance.jsx'
 // import OurProducts from './OurProducts'
 import Recommendations from '../global/Recommendations.jsx'
-// import PreviousRecommendations from './PreviousRecommendations'
-// import TheResults from './Performance.jsx'
 import BacktestedPerformance from '../global/BacktestedPerformance.jsx'
-// import PricingTable from './PricingTable'
 import Pricing from '../global/Pricing.jsx'
 import HowWeBeatTheMarket from '../global/HowWeBeatTheMarket.jsx'
 import MoreInformation from './MoreInformation.jsx'
-// import RewardVSRisk from './RewardVSRisk'
 import RiskReward from '../global/RiskReward.jsx'
 import Quote from './Quote'
 import Comparisons from '../global/Comparisons.jsx'
 import CumulativeInterest from './CumulativeInterest'
-import Newsletter from './Newsletter'
 import AboutUs from './AboutUs'
-import ContactUs from './ContactUs'
 import BottomCTA from '../global/BottomCTA.jsx'
 import Footer from './Footer'
 
@@ -57,32 +46,20 @@ const Home = React.createClass({
         <Hero/>
         <WhatIsIt/>
         <HowItWorks/>
-        {/* <AboveAverageReturns/> */}
-        {/* <ReachYourGoals/> */}
-        {/* <InformationalAdvantage/> */}
-        {/* <OneDollar/> */}
-        {/* <HigherPerformance/> */}
         <OneDollar/>
         <PilotTest/>
         <Performance/>
         {/* <OurProducts/> */}
-        {/* <PreviousRecommendations/> */}
         <Recommendations/>
-        {/* <TheResults/> */}
         <BacktestedPerformance/>
-        {/* <PricingTable/> */}
         <Pricing/>
         <HowWeBeatTheMarket/>
-        {/* <MoreInformation /> */}
-        {/* <RewardVSRisk/> */}
         <RiskReward/>
         <Comparisons/>
         <Quote/>
         <CumulativeInterest/>
-        {/* <Newsletter/> */}
         <AboutUs/>
         <BottomCTA/>
-        {/* <ContactUs/> */}
         <Footer/>
         {this.props.children}
       </div>
