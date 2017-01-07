689203cf358151c7e27e016d5a6d7e7508f88f7d
Mark Lyck <mark.lyck@gmail.com>
2017-01-07 19:12:54 +0100

    pilot test text update

--- app/scripts/components/home/PilotTest.jsx
+++ app/scripts/components/home/PilotTest.jsx
@@ -11,8 +11,8 @@ class PilotTest extends React.Component {
             <p>
               Formula Stocks has undertaken a 3-year pilot program. Performance data was recorded
               under normal real-time market conditions with capital, and financial results
-              reviewed by a state-licensed auditor. Based on the BUSINESS method, we recorded an
-              average return on equity employed to sustain securities trading of +66.54% in 2009,
+              reviewed by a state-licensed auditor. We recorded an average return on equity
+              employed to sustain securities trading of +66.54% in 2009,
               +52.56% in 2010, +16.84% in 2011.<sup>*</sup><br/><br/>
             </p>
             <p className="disclaimer"><sup>*</sup>Past performance is no indication of future performance.</p>
