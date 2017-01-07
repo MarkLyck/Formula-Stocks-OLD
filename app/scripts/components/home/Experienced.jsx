dce365520695ab2d389b055dad8b03fc1009fd26
Mark Lyck <mark.lyck@gmail.com>
2017-01-07 22:55:06 +0100

    Experienced size and content changes

--- app/scripts/components/home/Experienced.jsx
+++ app/scripts/components/home/Experienced.jsx
@@ -5,7 +5,7 @@ class Experienced extends React.Component {
   render() {
     return (
       <section className="experienced section">
-        <h2>Are you an experienced, professional or institutional investor? <Link to="/pro">Click here</Link></h2>
+        <h2>Are you an experienced, professional or institutional investor? <Link to="/pro">Visit our professional site instead</Link></h2>
       </section>
     )
   }
