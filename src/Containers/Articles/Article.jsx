6604cfd559a7ff608eedbb67bb630a30092ba992
Mark Lyck <mark.lyck@gmail.com>
2017-02-02 01:57:46 +0100

    Articles can be edited

--- src/Containers/Articles/Article.jsx
+++ src/Containers/Articles/Article.jsx
@@ -21,7 +21,10 @@ class Article extends React.Component {
     e.preventDefault()
     const markdown = this.refs.mdeditor.state.content
     store.articles.data.get(this.props.article._id).set('body', markdown)
+    if (this.refs.titleInput.value) { store.articles.data.get(this.props.article._id).set('title', this.refs.titleInput.value) }
+    if (this.refs.authorInput.value) { store.articles.data.get(this.props.article._id).set('author', this.refs.authorInput.value) }
     store.articles.data.get(this.props.article._id).save()
+    this.toggleEdit()
   }
 
   render() {
@@ -33,6 +36,8 @@ class Article extends React.Component {
         <div className="article" style={this.props.style}>
           <div className="header-image" style={{ backgroundImage: `url(${article.image._downloadURL})` }} alt="header"/>
           <form className="edit-form" onSubmit={this.submitEdit}>
+            <input className="title-input" placeholder={article.title} ref="titleInput"/>
+            <input className="author-input" placeholder={article.author} ref="authorInput"/>
             <MarkdownEditor initialContent={article.body} iconsSet="font-awesome" ref="mdeditor"/>
             <input type="submit" value="Publish"/>
           </form>
