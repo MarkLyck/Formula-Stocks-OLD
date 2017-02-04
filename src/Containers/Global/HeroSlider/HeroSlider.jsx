422df8fedecc8769861eedd8bc76035091dc24c7
Mark Lyck <mark.lyck@gmail.com>
2017-02-04 20:21:57 +0100

    Article improvements, start hero slider

---
+++ src/Containers/Global/HeroSlider/HeroSlider.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Hero extends React.Component {
+  render() {
+    return (
+      <div className="hero-slider">
+
+      </div>
+    )
+  }
+}
+
+export default Hero
