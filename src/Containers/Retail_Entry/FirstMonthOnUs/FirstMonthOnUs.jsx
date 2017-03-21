c295e8456a6b2e30528876414b84785f48686d5d
MarkLyck <mark.lyck@gmail.com>
2017-03-21 17:20:27 +0100

    First month dumb component

--- src/Containers/Retail_Entry/FirstMonthOnUs/FirstMonthOnUs.jsx
+++ src/Containers/Retail_Entry/FirstMonthOnUs/FirstMonthOnUs.jsx
@@ -3,22 +3,18 @@ import Scroll from 'react-scroll'
 import { Link } from 'react-router'
 import './firstMonthOnUs.css'
 
-class FirstMonthOnUs extends React.Component {
-  render() {
-    const Element = Scroll.Element
-    return (
-      <section className="first-month-on-us section">
-        <Element name="pricing"/>
-        <h2 className="title">The first month is on us</h2>
-        <div className="divider"/>
-        <p>
-          If you decide Formula Stocks isn't for you - simply cancel online at any time without obligations.
-        </p>
-        <h2 className="cost">$50 monthly after 30 days</h2>
-        <Link to="/signup" className="signup-cta">Join free for a month</Link>
-      </section>
-    )
-  }
-}
+const Element = Scroll.Element
+const FirstMonthOnUs = () => (
+  <section className="first-month-on-us section">
+    <Element name="pricing"/>
+    <h2 className="title">The first month is on us</h2>
+    <div className="divider"/>
+    <p>
+      If you decide Formula Stocks isn't for you - simply cancel online at any time without obligations.
+    </p>
+    <h2 className="cost">$50 monthly after 30 days</h2>
+    <Link to="/signup" className="signup-cta">Join free for a month</Link>
+  </section>
+)
 
 export default FirstMonthOnUs
