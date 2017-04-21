ab5397fc458f7d2a549280fed5ad0fc7b82ae38e
MarkLyck <mark.lyck@gmail.com>
2017-04-21 20:24:13 +0200

    Fix admin users

--- src/actions/admin.js
+++ src/actions/admin.js
@@ -3,14 +3,15 @@ import { anomToken } from '../store'
 export const FETCHING_USERS = 'FETCHING_USERS'
 export const RECEIVE_USERS = 'RECEIVE_USERS'
 
-let fetchHeaders = new Headers();
-const authToken = localStorage.getItem('authtoken') ? localStorage.authtoken : anomToken
-fetchHeaders.append('Authorization', `Kinvey ${authToken}`)
-let options = { method: 'GET', headers: fetchHeaders }
-
 export function fetchUsers() {
   return (dispatch) => {
     dispatch(fetchingUsers())
+
+    let fetchHeaders = new Headers();
+    const authToken = localStorage.getItem('authtoken') ? localStorage.authtoken : anomToken
+    fetchHeaders.append('Authorization', `Kinvey ${authToken}`)
+    let options = { method: 'GET', headers: fetchHeaders }
+
     fetch(`https://baas.kinvey.com/user/kid_rJRC6m9F/`, options)
       .then(response => response.json())
       .then(json => dispatch(receiveUsers(json)))
